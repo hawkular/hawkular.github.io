c2a85c52c7389b0b23c24b20b412eeb6e690036f
Jirka Kremser <jkremser@redhat.com>
2015-03-12 02:13:30 +0100

    Swagger adoc files are download prior to each jBaking from branch called swagger

---
+++ .travis.swagger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REPO="hawkular/hawkular.github.io"
+BRANCH="swagger"
+DOC_PATH="src/main/jbake/content/docs/rest/"
+
+mkdir -p $DOC_PATH
+FILES=`curl -Ls https://api.github.com/repos/$REPO/contents/?ref=$BRANCH | grep download_url | cut -d '"' -f4`
+for file in $FILES; do
+  wget -P $DOC_PATH $file
+done
+
