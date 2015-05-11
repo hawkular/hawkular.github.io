7cd881dc0776bff7f48a3b6022366d706f18d7da
Jirka Kremser <jkremser@redhat.com>
2015-05-11 19:25:18 +0200

    Don't build empty documentation.

--- .travis.swagger.sh
+++ .travis.swagger.sh
@@ -20,8 +20,9 @@ addHeaders() {
   dir_path=$1
 
   for file in $(find $dir_path -maxdepth 1 -mindepth 1); do
-    # it assumes the filenames to be in "rest-${component}.adoc" form
+    [[ -s $file ]] || continue;
 
+    # it assumes the filenames to be in "rest-${component}.adoc" form
     title=`echo $(basename $file) | cut -d'.' -f1 | cut -d'-' -f2`
     title="$(tr '[:lower:]' '[:upper:]' <<< ${title:0:1})${title:1} REST API"
     echo $title
