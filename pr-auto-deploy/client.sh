386bda3f254f59d4265988dae912bfd3c06d0f50
Jirka Kremser <jkremser@redhat.com>
2015-09-01 00:15:11 +0200

    typos

--- pr-auto-deploy/client.sh
+++ pr-auto-deploy/client.sh
@@ -16,13 +16,13 @@
 # limitations under the License.
 #
 
-[ "$#" -ne 1 ] && {
+[ "$#" -ne 2 ] && {
   echo "usage: $0 <pr_number> <host>"
   exit 1
 }
 
-$PR_NUMBER=$1
-$HOST=$2
-$BASE_PORT=10000
+PR_NUMBER=$1
+HOST=$2
+BASE_PORT=10000
 
 echo -n "$PR_DEPLOY_TOKEN $PR_NUMBER" | nc -4 -w1 $HOST $BASE_PORT
